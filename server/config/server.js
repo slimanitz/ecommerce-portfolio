e1814a068f62ad27d79990afbef35992a47bbff9
slimanePLF <slimaneber2001@gmail.com>
2022-09-09 13:14:55 +0200

    Implementing morgan and cors

--- server/config/server.js
+++ server/config/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
 const router = require('../api/routes');
 const { errorHandler } = require('../middlewares/error');
 
 const app = express();
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(express.json());
 app.use('/api', router);
+app.use(cors());
 app.use(errorHandler);
 
 module.exports = app;
