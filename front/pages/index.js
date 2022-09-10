a46da231bbbfa9290e5f6e58648ce0288fa232f5
slimanePLF <slimaneber2001@gmail.com>
2022-09-10 20:48:40 +0200

    Auto play and carousel up and running

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,52 +1,63 @@
-import Image from 'next/image'
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+/* eslint-disable @next/next/no-img-element */
+import { Pagination, Scrollbar, A11y, EffectFade, Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
+import 'swiper/css/effect-fade'
+import styles from '../styles/Index.module.css'
 
 export default function Home() {
   return (
     <>
-      <div className="container-fluid mt-5">
+      <div className="container-fluid ">
         <div className="row">
           <Swiper
             // install Swiper modules
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            modules={[Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
             spaceBetween={50}
             slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
+            effect="fade"
+            pagination={{
+              clickable: true,
+              type: 'bullets',
+            }}
             onSwiper={(swiper) => console.log(swiper)}
             onSlideChange={() => console.log('slide change')}
           >
             <SwiperSlide>
-              <Image
-                src={'/carousel-1.jpg'}
-                layout="fixed"
-                width={200}
-                height={200}
-                alt=""
-              />
+              <div className={`${styles.carouselBg}  h-100 row mb-3`}>
+                <img
+                  src={
+                    'https://www.transparentpng.com/thumb/coffee/bEnole-coffee-heart-free-png.png'
+                  }
+                  alt=""
+                  className={`${styles.carouselImage}`}
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-              <Image
-                src={'/carousel-2.jpg'}
-                layout="fixed"
-                width={200}
-                height={200}
-                alt=""
-              />
+              <div className={`${styles.carouselBg}  h-100 row mb-3`}>
+                <img
+                  src={
+                    'https://www.transparentpng.com/thumb/coffee/v6EtCB-coffee-transparent-background.png'
+                  }
+                  alt=""
+                  className={`${styles.carouselImage}`}
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-              <Image
-                src={'/carousel-3.jpg'}
-                layout="fixed"
-                width={200}
-                height={200}
-                alt=""
-              />
+              <div className={`${styles.carouselBg}  h-100 row mb-3`}>
+                <img
+                  src={
+                    'https://www.transparentpng.com/thumb/coffee/TgK6AC-coffee-transparent-image.png'
+                  }
+                  alt=""
+                  className={`${styles.carouselImage}`}
+                />
+              </div>
             </SwiperSlide>
           </Swiper>
         </div>
