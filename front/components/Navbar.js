676a63b0859dcc05f7462a37540a8e0b3961a864
slimanePLF <slimaneber2001@gmail.com>
2022-09-13 22:19:39 +0200

    Fixing width problems

--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -3,56 +3,58 @@ import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <div className=" mt-3">
-      <div className="row">
-        <div className="col-sm justify-content-center">
-          <div className="input-group  my-5  w-50  mx-auto">
-            <input
-              type="text"
-              className={`form-control ${styles.seachInput}`}
-              placeholder="Search"
-            />
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              id="button-addon2"
-            >
-              <i className="bi bi-search "></i>
+    <div className="container-fluid">
+      <div className=" mt-3">
+        <div className="row">
+          <div className="col-sm justify-content-center">
+            <div className="input-group  my-5  w-50  mx-auto">
+              <input
+                type="text"
+                className={`form-control ${styles.seachInput}`}
+                placeholder="Search"
+              />
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+              >
+                <i className="bi bi-search "></i>
+              </button>
+            </div>
+          </div>
+          <div className="col-sm justify-content-center " align={'center'}>
+            <Image src={'/logo.png'} layout="fixed" width={200} height={200} />
+          </div>
+          <div className="col-sm   text-center ">
+            <button className={`btn  my-5 btn-xl ${styles.cart}`}>
+              <i className="bi bi-cart"></i>
             </button>
           </div>
         </div>
-        <div className="col-sm justify-content-center " align={'center'}>
-          <Image src={'/logo.png'} layout="fixed" width={200} height={200} />
+        <div className={`row mt-3 py-4 px-0 ${styles.navRow}`}>
+          <ul className="nav justify-content-center">
+            <li className="nav-item mr-lg-4">
+              <a className={`nav-link ${styles.customLink}`} href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item mx-lg-4">
+              <a className={`nav-link ${styles.customLink}`} href="#">
+                Product
+              </a>
+            </li>
+            <li className="nav-item mx-lg-4">
+              <a className={`nav-link ${styles.customLink}`} href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item ml-lg-4">
+              <a className={`nav-link ${styles.customLink}`} href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="col-sm   text-center ">
-          <button className={`btn  my-5 btn-xl ${styles.cart}`}>
-            <i className="bi bi-cart"></i>
-          </button>
-        </div>
-      </div>
-      <div className={`row mt-3 py-4 px-0 ${styles.navRow}`}>
-        <ul className="nav justify-content-center">
-          <li className="nav-item mr-4">
-            <a className={`nav-link ${styles.customLink}`} href="#">
-              Home
-            </a>
-          </li>
-          <li className="nav-item mx-4">
-            <a className={`nav-link ${styles.customLink}`} href="#">
-              Product
-            </a>
-          </li>
-          <li className="nav-item mx-4">
-            <a className={`nav-link ${styles.customLink}`} href="#">
-              About
-            </a>
-          </li>
-          <li className="nav-item ml-4">
-            <a className={`nav-link ${styles.customLink}`} href="#">
-              Contact
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
   )
--- front/pages/index.js
+++ front/pages/index.js
@@ -10,132 +10,130 @@ import styles from '../styles/Index.module.css'
 
 export default function Home() {
   return (
-    <>
-      <div className="container-fluid ">
-        <div className="row">
-          <Swiper
-            // install Swiper modules
-            modules={[Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
-            spaceBetween={50}
-            slidesPerView={1}
-            autoplay
-            effect="fade"
-            pagination={{
-              clickable: true,
-              type: 'bullets',
-            }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-          >
-            <SwiperSlide>
-              <div className={`${styles.carouselBg}  h-100 row mb-3`}>
-                <img
-                  src={
-                    'https://www.transparentpng.com/thumb/coffee/bEnole-coffee-heart-free-png.png'
-                  }
-                  alt=""
-                  className={`${styles.carouselImage}`}
-                />
+    <div className="container-fluid ">
+      <div className="row">
+        <Swiper
+          // install Swiper modules
+          modules={[Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
+          spaceBetween={50}
+          slidesPerView={1}
+          autoplay
+          effect="fade"
+          pagination={{
+            clickable: true,
+            type: 'bullets',
+          }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide>
+            <div className={`${styles.carouselBg}  h-100 row mb-3`}>
+              <img
+                src={
+                  'https://www.transparentpng.com/thumb/coffee/bEnole-coffee-heart-free-png.png'
+                }
+                alt=""
+                className={`${styles.carouselImage}`}
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={`${styles.carouselBg}  h-100 row mb-3`}>
+              <img
+                src={
+                  'https://www.transparentpng.com/thumb/coffee/v6EtCB-coffee-transparent-background.png'
+                }
+                alt=""
+                className={`${styles.carouselImage}`}
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={`${styles.carouselBg}  h-100 row mb-3`}>
+              <img
+                src={
+                  'https://www.transparentpng.com/thumb/coffee/TgK6AC-coffee-transparent-image.png'
+                }
+                alt=""
+                className={`${styles.carouselImage}`}
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="row text-center mt-5">
+        <p className={styles.title}>Welcome to ICOFFEE</p>
+      </div>
+      <div className="row text-center justify-content-center">
+        <p className="subTitle w-50">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
+          fermentum urna. Nulla facilisi. Integer rutrum augue et purus vehicula
+          maximus. Praesent eget quam ullamcorper, finibus sapien non, semper
+          mauris. Donec sed odio scelerisque arcu commodo pretium eget at velit.
+          Nulla mattis porta auctor. Curabitur et felis eget diam venenatis
+          dapibus. Pellentesque ligula purus, interdum ac nunc lobortis,
+          tincidunt porttitor mi.
+        </p>
+      </div>
+      <div className="row mx- mt-5 ">
+        <div className="col-2"></div>
+        <div className="col-8 ">
+          <div className="row">
+            <div className="col-4">
+              <div className="row">
+                <img src="/leaf.svg" width="50px" height="50px" />
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className={`${styles.carouselBg}  h-100 row mb-3`}>
-                <img
-                  src={
-                    'https://www.transparentpng.com/thumb/coffee/v6EtCB-coffee-transparent-background.png'
-                  }
-                  alt=""
-                  className={`${styles.carouselImage}`}
-                />
+              <div className="row text-center">
+                <p className={`${styles.cacTitle}`}>Always Fresh</p>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className={`${styles.carouselBg}  h-100 row mb-3`}>
-                <img
-                  src={
-                    'https://www.transparentpng.com/thumb/coffee/TgK6AC-coffee-transparent-image.png'
-                  }
-                  alt=""
-                  className={`${styles.carouselImage}`}
-                />
+              <div className="row text-center">
+                <p className={`${styles.cacsubTitle}`}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  a fermentum urna. Nulla facilisi. Integer rutrum augue et
+                  purus vehicula maximus. Praesent eget quam ullamcorper,
+                  finibus sapien
+                </p>
               </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-        <div className="row text-center mt-5">
-          <p className={styles.title}>Welcome to ICOFFEE</p>
-        </div>
-        <div className="row text-center justify-content-center">
-          <p className="subTitle w-50">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
-            fermentum urna. Nulla facilisi. Integer rutrum augue et purus
-            vehicula maximus. Praesent eget quam ullamcorper, finibus sapien
-            non, semper mauris. Donec sed odio scelerisque arcu commodo pretium
-            eget at velit. Nulla mattis porta auctor. Curabitur et felis eget
-            diam venenatis dapibus. Pellentesque ligula purus, interdum ac nunc
-            lobortis, tincidunt porttitor mi.
-          </p>
-        </div>
-        <div className="row mx- mt-5 ">
-          <div className="col-2"></div>
-          <div className="col-8 ">
-            <div className="row">
-              <div className="col-4">
-                <div className="row">
-                  <img src="/leaf.svg" width="50px" height="50px" />
-                </div>
-                <div className="row text-center">
-                  <p className={`${styles.cacTitle}`}>Always Fresh</p>
-                </div>
-                <div className="row text-center">
-                  <p className={`${styles.cacsubTitle}`}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec a fermentum urna. Nulla facilisi. Integer rutrum augue
-                    et purus vehicula maximus. Praesent eget quam ullamcorper,
-                    finibus sapien
-                  </p>
-                </div>
+            </div>
+            <div className="col-4">
+              <div className="row">
+                <img src="/health.svg" width="50px" height="50px" />
               </div>
-              <div className="col-4">
-                <div className="row">
-                  <img src="/health.svg" width="50px" height="50px" />
-                </div>
-                <div className="row text-center">
-                  <p className={`${styles.cacTitle}`}>Healthy</p>
-                </div>
-                <div className="row text-center">
-                  <p className={`${styles.cacsubTitle}`}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec a fermentum urna. Nulla facilisi. Integer rutrum augue
-                    et purus vehicula maximus. Praesent eget quam ullamcorper,
-                    finibus sapien
-                  </p>
-                </div>
+              <div className="row text-center">
+                <p className={`${styles.cacTitle}`}>Healthy</p>
               </div>
-              <div className="col-4">
-                <div className="row">
-                  <img src="/premium.svg" width="50px" height="50px" />
-                </div>
-                <div className="row text-center">
-                  <p className={`${styles.cacTitle}`}>Premium quality</p>
-                </div>
-                <div className="row text-center">
-                  <p className={`${styles.cacsubTitle}`}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec a fermentum urna. Nulla facilisi. Integer rutrum augue
-                    et purus vehicula maximus. Praesent eget quam ullamcorper,
-                    finibus sapien
-                  </p>
-                </div>
+              <div className="row text-center">
+                <p className={`${styles.cacsubTitle}`}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  a fermentum urna. Nulla facilisi. Integer rutrum augue et
+                  purus vehicula maximus. Praesent eget quam ullamcorper,
+                  finibus sapien
+                </p>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="row">
+                <img src="/premium.svg" width="50px" height="50px" />
+              </div>
+              <div className="row text-center">
+                <p className={`${styles.cacTitle}`}>Premium quality</p>
+              </div>
+              <div className="row text-center">
+                <p className={`${styles.cacsubTitle}`}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  a fermentum urna. Nulla facilisi. Integer rutrum augue et
+                  purus vehicula maximus. Praesent eget quam ullamcorper,
+                  finibus sapien
+                </p>
               </div>
             </div>
           </div>
-          <div className="col-2"></div>
         </div>
-        <div className="row">
-          <p className="text-center">Deals of the day</p>
-        </div>
+        <div className="col-2"></div>
       </div>
-    </>
+      <div className="row">
+        <p className="text-center">Deals of the day</p>
+      </div>
+    </div>
   )
 }
