ea210fd14fd58b3c9509b14ae4425cd7a781adc3
slimanePLF <slimaneber2001@gmail.com>
2022-09-17 12:22:47 +0200

    Adding static route to display pics

--- server/config/server.js
+++ server/config/server.js
@@ -7,6 +7,7 @@ const { errorHandler } = require('../middlewares/error');
 
 const app = express();
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use('/public', express.static('./uploads'));
 app.use(express.json());
 app.use('/api', router);
 app.use(cors());
