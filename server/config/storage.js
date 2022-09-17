00f28a8b837fc975b42031615536b6d5af95636d
slimanePLF <slimaneber2001@gmail.com>
2022-09-17 12:22:30 +0200

    Setting up storage

---
+++ server/config/storage.js
@@ -0,0 +1,13 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, path.join(__dirname, '../uploads/'));
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${file.originalname.match(/\..*$/)[0]}`);
+  },
+});
+
+module.exports = storage;
