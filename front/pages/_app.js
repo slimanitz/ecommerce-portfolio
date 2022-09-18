53883f400a406f6e30f904c19405755d4877340e
slimanePLF <slimaneber2001@gmail.com>
2022-09-18 22:43:10 +0200

    Redux setup

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,20 +1,22 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { wrapper, store } from '../store'
 import Layout from '../components/Layout'
 import Head from 'next/head'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </Provider>
   )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
