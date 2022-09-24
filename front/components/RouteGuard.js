d7a39ecd1b479fd25e6bd7f2bfd5a7a35b88fca9
slimanePLF <slimaneber2001@gmail.com>
2022-09-25 01:20:43 +0200

    Route guard enhanced

--- front/components/RouteGuard.js
+++ front/components/RouteGuard.js
@@ -1,47 +1,49 @@
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 
-export { RouteGuard }
+export { RouteGuard };
 
 function RouteGuard({ children }) {
-  const router = useRouter()
-  const [authorized, setAuthorized] = useState(false)
-  const userReducer = useSelector((state) => state.userReducer)
+  const router = useRouter();
+  const [authorized, setAuthorized] = useState(false);
+  const userReducer = useSelector((state) => state.userReducer);
 
   useEffect(() => {
     // on initial load - run auth check
-    authCheck(router.asPath)
+    authCheck(router.asPath);
 
     // on route change start - hide page content by setting authorized to false
-    const hideContent = () => setAuthorized(false)
-    router.events.on('routeChangeStart', hideContent)
+    const hideContent = () => setAuthorized(false);
+    router.events.on('routeChangeStart', hideContent);
 
     // on route change complete - run auth check
-    router.events.on('routeChangeComplete', authCheck)
+    router.events.on('routeChangeComplete', authCheck);
 
     // unsubscribe from events in useEffect return function
     return () => {
-      router.events.off('routeChangeStart', hideContent)
-      router.events.off('routeChangeComplete', authCheck)
-    }
+      router.events.off('routeChangeStart', hideContent);
+      router.events.off('routeChangeComplete', authCheck);
+    };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userReducer.user])
+  }, [userReducer.token]);
 
   function authCheck(url) {
     // redirect to login page if accessing a private page and not logged in
-    const publicPaths = ['/login', '/']
-    const path = url.split('?')[0]
-    if (!userReducer.user && !publicPaths.includes(path)) {
-      setAuthorized(false)
+    const publicPaths = ['/login', '/'];
+    const path = url.split('?')[0];
+    if (!userReducer.token && !publicPaths.includes(path)) {
+      setAuthorized(false);
+
       router.push({
         pathname: '/login',
-      })
+        query: { redirect: path },
+      });
     } else {
-      setAuthorized(true)
+      setAuthorized(true);
     }
   }
 
-  return authorized && children
+  return authorized && children;
 }
