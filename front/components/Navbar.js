1207b769520f28908830aa5f4849d9ef5567076b
slimanitz <slimaneber2001@gmail.com>
2022-10-02 00:14:06 +0200

    Quick fix

--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -1,52 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image';
-import Link from 'next/link';
-import { useSelector } from 'react-redux';
-import styles from '../styles/Navbar.module.css';
-import Nav from 'react-bootstrap/Nav';
-import { useRouter } from 'next/router';
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "../styles/Navbar.module.css";
+import Nav from "react-bootstrap/Nav";
+import { useRouter } from "next/router";
+import allActions from "../redux/actions";
 
 export default function Navbar() {
   const cart = useSelector((state) => state.cartReducer.cart);
   const userReducer = useSelector((state) => state.userReducer);
   const router = useRouter();
+  const dispatch = useDispatch();
+
+  const disconnect = () => {
+    router.push("/");
+    dispatch(allActions.userActions.logOut());
+  };
 
   return (
     <div className="container-fluid">
-      {userReducer.user && (
-        <div className="row">
-          <div className=" col-3 mr-auto">
-            <p>Logged in as: {userReducer.user?.fullName}</p>
-          </div>
-        </div>
-      )}
-
       <div className=" mt-3">
         <div className="row">
           <div className="col-sm justify-content-center">
             <div className="input-group  my-5  w-50  mx-auto">
-              <input
-                type="text"
-                className={`form-control ${styles.seachInput}`}
-                placeholder="Search"
-              />
-              <button
-                className="btn btn-outline-secondary"
-                type="button"
-                id="button-addon2"
-              >
-                <i className="bi bi-search "></i>
-              </button>
+              {userReducer.user ? (
+                <div className="row">
+                  <div className=" col- mx-auto">
+                    <p>Hello {userReducer.user?.fullName}</p>
+                  </div>
+                  <div className="col- mx-auto">
+                    <button className="btn btn-danger" onClick={disconnect}>
+                      Disconnect
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="row">
+                  <button
+                    className="btn btn-primary"
+                    onClick={() =>
+                      router.push("/login", { query: { redirect: "/" } })
+                    }
+                  >
+                    Se connecter
+                  </button>
+                </div>
+              )}
             </div>
           </div>
-          <div className="col-sm justify-content-center " align={'center'}>
-            <img src={'/logo.png'} width={200} height={200} alt="" />
+          <div className="col-sm justify-content-center " align={"center"}>
+            <img src={"/logo.png"} width={200} height={200} alt="" />
           </div>
 
           <div className="cart col-sm   text-center">
             <button
               type="button"
-              onClick={() => router.push('/cart')}
+              onClick={() => router.push("/cart")}
               className={`btn  my-5 ${styles.cart} p-3`}
             >
               <img src="/cart.svg" height={30} width={30} alt=""></img>
@@ -59,7 +68,7 @@ export default function Navbar() {
             <li className="nav-item mr-lg-4">
               <Link className={`nav-link ${styles.customLink}`} href="/">
                 <Nav.Link
-                  as={'a'}
+                  as={"a"}
                   className={`nav-link ${styles.customLink}`}
                   href="/"
                 >
@@ -70,7 +79,7 @@ export default function Navbar() {
             <li className="nav-item mx-lg-4">
               <Link href="/product">
                 <Nav.Link
-                  as={'a'}
+                  as={"a"}
                   className={`nav-link ${styles.customLink}`}
                   href="/product"
                 >
@@ -81,7 +90,7 @@ export default function Navbar() {
             <li className="nav-item mx-lg-4">
               <Link className={`nav-link ${styles.customLink}`} href="/about">
                 <Nav.Link
-                  as={'a'}
+                  as={"a"}
                   className={`nav-link ${styles.customLink}`}
                   href="/about"
                 >
@@ -92,7 +101,7 @@ export default function Navbar() {
             <li className="nav-item ml-lg-4">
               <Link className={`nav-link ${styles.customLink}`} href="/contact">
                 <Nav.Link
-                  as={'a'}
+                  as={"a"}
                   className={`nav-link ${styles.customLink}`}
                   href="/contact"
                 >
