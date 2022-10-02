d9db3b84bf513a6fa8ea9bea5dc159b33ef07ce1
slimanitz <slimaneber2001@gmail.com>
2022-10-02 18:51:35 +0200

    Quick fixes

--- front/axios.js
+++ front/axios.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 const API = axios.create({
   baseURL: `http://localhost:8080/api`,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
-})
+});
 
-export default API
+export default API;
