cd9362b0e543eea103f8772a20398001cba1551d
slimanitz <slimaneber2001@gmail.com>
2022-10-06 22:46:01 +0200

    Cart getting products data

--- front/pages/cart.js
+++ front/pages/cart.js
@@ -1,11 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
+import API from "../axios";
+import { wrapper, store } from "../redux/store";
 
 export default function Cart() {
-  const cart = useSelector((state) =>
-    state.productReducer.products.filter((e) =>
-      state.cartReducer.cart.some((p) => e._id === p._id)
-    )
+  const cart = useSelector((state) => state.cartReducer.cart);
+  const [cartProducts, setCartProducts] = useState({});
+
+  useEffect(() => {
+    API.get("/products/bulk", {
+      params: { productIds: JSON.stringify(cart) },
+    }).then((response) => setCartProducts(response.data));
+  }, []);
+
+  return (
+    <div>
+      <p>Cart: {JSON.stringify(cart)}</p>
+      <p>products: {JSON.stringify(cartProducts)}</p>
+    </div>
   );
-  const tmp = useSelector((state) => console.log(state.cartReducer.cart));
-  return <div>{JSON.stringify(cart)}</div>;
 }
