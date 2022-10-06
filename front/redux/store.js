9048cdcda71d247d89b992d2a96959bc8530cf99
slimanitz <slimaneber2001@gmail.com>
2022-10-06 22:47:09 +0200

    New redux implementation

--- front/redux/store.js
+++ front/redux/store.js
@@ -1,22 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
-import thunk from 'redux-thunk'
-import { createWrapper } from 'next-redux-wrapper'
-import rootReducer from './reducers'
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { createWrapper } from "next-redux-wrapper";
+import rootReducer from "./reducers";
 
 // initial states here
-const initalState = { cart: [], user: [] }
+const initalState = { cart: [], user: [] };
 
 // middleware
-const middleware = [thunk]
+const middleware = [thunk];
 
 // creating store
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(middleware),
-})
+});
 
 // assigning store to next wrapper
-const makeStore = () => store
+const makeStore = () => store;
 
-export const wrapper = createWrapper(makeStore)
+export const wrapper = createWrapper(makeStore);
