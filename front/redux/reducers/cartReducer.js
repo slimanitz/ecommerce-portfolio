2ede8fb710a7493e74ad7504ca14a4acd726c71f
slimanitz <slimaneber2001@gmail.com>
2022-10-06 22:46:32 +0200

    new cart pattern

--- front/redux/reducers/cartReducer.js
+++ front/redux/reducers/cartReducer.js
@@ -1,9 +1,7 @@
 const currentCart = (state = { cart: [] }, action) => {
   switch (action.type) {
-    case 'ADD_TO_CART':
-      const index = state.cart.findIndex(
-        (e) => e._id === action.payload.product
-      );
+    case "ADD_TO_CART":
+      const index = state.cart.findIndex((e) => e._id === action.payload._id);
       return {
         ...state,
         cart:
@@ -11,18 +9,18 @@ const currentCart = (state = { cart: [] }, action) => {
             ? [
                 ...state.cart,
                 {
-                  _id: action.payload.product,
+                  _id: action.payload._id,
                   quantity: action.payload.quantity,
                 },
               ]
             : state.cart.map((e) =>
-                e._id === action.payload.product
+                e._id === action.payload._id
                   ? { ...e, quantity: e.quantity + action.payload.quantity }
                   : e
               ),
       };
 
-    case 'REMOVE_FROM_CART':
+    case "REMOVE_FROM_CART":
       return {
         ...state,
         cart: state.cart.filter((e) => e._id === action.payload),
