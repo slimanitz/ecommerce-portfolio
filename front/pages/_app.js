591a2bf28d3f41e3cca12e55005fd4f3c4df7c91
slimanitz <slimaneber2001@gmail.com>
2022-10-06 22:45:31 +0200

    New wrapper for redux

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,14 +1,16 @@
-import '../styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { wrapper, store } from '../redux/store';
-import Layout from '../components/Layout';
-import Head from 'next/head';
-import { Provider } from 'react-redux';
-import { RouteGuard } from '../components/RouteGuard';
-import 'react-multi-carousel/lib/styles.css';
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { wrapper, store } from "../redux/store";
+import Layout from "../components/Layout";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { RouteGuard } from "../components/RouteGuard";
+import "react-multi-carousel/lib/styles.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, ...rest }) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  console.log(store.getState());
   return (
     <Provider store={store}>
       <Head>
@@ -16,11 +18,11 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <Layout>
         <RouteGuard>
-          <Component {...pageProps} />
+          <Component {...props.pageProps} />
         </RouteGuard>
       </Layout>
     </Provider>
   );
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
